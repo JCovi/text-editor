910d0c2c0ad90a44514f9f159ce5a644c145caec
JCovi <joshpcovington@gmail.com>
2024-02-10 15:38:16 -0700

    instal.js file code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,33 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// TODO: Add an event handler to the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {  
+    console.log('beforeinstallprompt');
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+event.preventDefault();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.deferredPrompt = event;});
+
+// TODO: Implement a click event handler on the butInstall element
+butInstall.addEventListener('click', async () => {
+
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      return;
+    }
+    promptEvent.prompt();
+    // Wait for the user to respond to the prompt
+    const choiceResult = await promptEvent.userChoice;
+    console.log('User choice:', choiceResult);
+    // Clear the deferredPrompt variable
+    window.deferredPrompt = null;
+
+
+});
+
+
+// TODO: Add an handler for the appinstalled event
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed:', event);
+});
